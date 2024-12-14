2517647f4075ddebb059d12bb91c21ffcb4ccb56
Anna Fedyna <annafedyna03@gmail.com>
2024-12-14 11:55:46 +0000

    decrement button

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -7,14 +7,26 @@ function increment(node) {
   node.textContent = Number(current) + 1;
 }
 
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
+}
+
 export function App() {
   const header = makeHeader();
   const main = makeMain();
 
   const body = document.createElement("body");
   body.append(header, main);
+
   const button = body.querySelector("#increment");
   const counter = body.querySelector("#counter");
+  const buttonDecrement = body.querySelector('#decrement');
+
+  buttonDecrement.addEventListener('click', () => {
+    decrement(counter);
+  })
+
   button.addEventListener("click", () => {
     increment(counter);
   });
