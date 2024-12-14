36feb9f3aabc5748a637bf7261bed90364fdf7e1
eediallo <elhhabdouldiallo@gmail.com>
2024-12-14 11:52:26 +0000

    import header and main

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -1,3 +1,6 @@
+import { makeHeader } from "./header.js";
+import { makeMain } from "./main.js";
+
 //increments the number in a node's text
 function increment(node) {
   let current = node.textContent;
@@ -5,22 +8,10 @@ function increment(node) {
 }
 
 export function App() {
+  const header = makeHeader();
+  const main = makeMain();
   const body = document.createElement("body");
-
-  const header = document.createElement("header");
-  header.innerHTML = `
-        <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
-    `;
-  body.appendChild(header);
-
-  const main = document.createElement("main");
-  main.innerHTML = `
-        <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
-    `;
-  body.appendChild(main);
-
+  body.append(header, main);
   const button = body.querySelector("#increment");
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
